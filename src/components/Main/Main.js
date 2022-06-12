f244ebf9025ffbcdd908ffdbb31809046c9ecdc7
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-06-12 10:56:31 +0500

    fix: fixed fonts

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Lead from './Lead/Lead';
 import Intro from './Intro/Intro';
 import Photos from './Photos/Photos';
+import Places from './Places/Places';
 
 function Main() {
   return (
---
+++ src/components/Main/Places/Places.js
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './Places.module.css';
+
+function Places() {
+  return (
+    <section class={s.places}>
+        
+    </section>
+  );
+}
+
+export default Places;
